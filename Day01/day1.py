8d97f492e1257f9b3b2e61fff6bbea6cd9947224
bparrill <bparrill+gh@gmail.com>
2018-12-05 10:21:44 -0600

    Day 1 Completed

--- Day01/day1.py
+++ Day01/day1.py
@@ -2,7 +2,27 @@
 
 # Read data file, slit lines into a list
 with open('input.txt', 'r') as f:
-    data = f.read().split()
+    strData = f.read().split()
 
+# Convert from string to int
+data = [int(i) for i in strData]
+
+# Part 1
 # Convert list of strings to integer, then add them all up
-print(f'Resulting frequency is: {sum([int(i) for i in data])}')
+print(f'Resulting frequency is: {sum(data)}')
+
+# Part 2
+# Keep a running list of frequencies, look for the first frequency it reaches twice
+freqList = []
+curFreq  = 0
+found    = False
+loops    = 0
+while not found:
+    for i in data:
+        curFreq = curFreq + i
+        if (curFreq in freqList) and (not found):
+            print(f'Frequency {curFreq} has previously been reached')
+            found = True
+        freqList.append(curFreq)
+    loops = loops + 1
+    print(f'Completed {loops} loops through the data.  Frequency list at {len(freqList)} items.')
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# AdventOfCode2018
+# AdventOfCode - 2018
+See [Advent of Code](https://adventofcode.com/) for details for the daily challenges.
